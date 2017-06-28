f3d5bcc3f800c2eceeeec2b8d07f5661471ddb28
samzoll22 <sam.zoll22@gmail.com>
2017-06-27 23:36:33 -0700

    implement initial page layout w/ bootstrap

---
+++ react-client/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+const Nav = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <h1>Simplr</h1>
+      </Navbar.Brand>
+    </Navbar.Header>
+  </Navbar>
+)
+
+export default Nav;
+
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import { Row, Col } from 'react-bootstrap';
+import Nav from './components/Nav.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+    return (
+    <div>
+      <Nav />
+      <Row>
+         <Col xs={12} md={6} mdOffset={3}>
+           <h1 className="headerText">LET'S GET STARTED</h1>
+         </Col>
+       </Row>
     </div>)
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+  //   <div>
+  //     <Grid>
+  //       <Nav />
+  //       <Row>
+  //         <Col xs={12} md={6} mdOffset={3}>
+  //           <h1>LET'S GET STARTED</h1>
+  //         </Col>
+  //       </Row>
+  //     </Grid>
+  //   </div>)
+  // }
